0b7c82ba048f15d38ea76af2661eefa060fd768d
chasju <91468816+chasju@users.noreply.github.com>
2022-12-05 17:37:18 +0100

    Menu toggle

--- src/js/listeners/menuToggle.mjs
+++ src/js/listeners/menuToggle.mjs
@@ -4,14 +4,12 @@ export function setMenuListener() {
 
   burgerOpen.addEventListener("click", () => {
     const menu = document.querySelector("#menu");
-    console.log(menu);
 
     menu.classList.add("d-block");
   });
 
   burgerClose.addEventListener("click", () => {
     const menu = document.querySelector("#menu");
-    console.log(menu);
 
     menu.classList.remove("d-block");
   });
