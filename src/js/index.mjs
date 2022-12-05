876e50913bc014da2841615cb5c7ce5c63002742
chasju <91468816+chasju@users.noreply.github.com>
2022-12-05 17:41:54 +0100

    Routing getPosts

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,10 +1,13 @@
 import * as listeners from "./listeners/index.mjs";
+import * as posts from "../js/api/posts/index.mjs";
+import { load } from "./storage/load.mjs";
 
 const path = location.pathname;
 
 if (path === "/" || path === "/index.html") {
   listeners.setMenuListener();
   listeners.logoutListener();
+  posts.getPosts();
 }
 if (path === "/register/" || path === "/register/index.html") {
   listeners.setRegisterFormListener();
@@ -12,3 +15,9 @@ if (path === "/register/" || path === "/register/index.html") {
 if (path === "/login/" || path === "/login/index.html") {
   listeners.setLoginListener();
 }
+
+// if (load("token") !== null || load("token") !== undefined) {
+//   console.log(`Email address exists`);
+// } else {
+//   console.log(`Email address not found`);
+// }
