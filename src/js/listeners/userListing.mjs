3a628d983948014ee93690dd1715ef60137997b0
chasju <91468816+chasju@users.noreply.github.com>
2022-12-07 08:46:46 +0100

    Adds functions to fetch for specific listing page

---
+++ src/js/listeners/userListing.mjs
@@ -0,0 +1,42 @@
+import { getPost } from "../api/posts/get.mjs";
+import { renderBidsTemplate } from "../templates/bids.mjs";
+import { renderUserListingTemplate } from "../templates/userListing.mjs";
+
+const url = new URL(location.href);
+const id = url.searchParams.get("id");
+
+/**
+ * Function that will GET a single post.
+ *
+ * @returns Gets post by id and displays a single result.
+ *
+ */
+
+export async function getSinglePost() {
+  try {
+    let post = await getPost(id);
+    post = [post];
+
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
+    renderUserListingTemplate(post, container);
+  } catch (error) {
+    console.log(error);
+    container.innerHTML = "<div><p>There was an error loading the content<p></div>" + error;
+  }
+}
+
+export async function getBids() {
+  try {
+    const posts = await getPost(id);
+    const bids = posts.bids;
+
+    const container = document.querySelector("#bidContainer");
+    document.title = `${posts.title} | Find.no`;
+    container.innerHTML = "";
+    renderBidsTemplate(bids, container);
+  } catch (error) {
+    console.log(error);
+    container.innerHTML = "<div><p>There was an error loading the content<p></div>" + error;
+  }
+}
