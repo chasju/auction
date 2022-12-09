b8abee4f709aa5cdd2e2abce4b0bab7583fc6b3a
chasju <91468816+chasju@users.noreply.github.com>
2022-12-09 15:39:16 +0100

    Fix links

--- src/js/listeners/menuToggle.mjs
+++ src/js/listeners/menuToggle.mjs
@@ -1,11 +1,19 @@
+import { getUserProfile } from "../api/profiles/get.mjs";
+import { load } from "../storage/load.mjs";
+
 export function setMenuListener() {
   const burgerOpen = document.querySelector("#hamburger");
   const burgerClose = document.querySelector("#inside-hamburger");
 
-  burgerOpen.addEventListener("click", () => {
+  burgerOpen.addEventListener("click", async () => {
     const menu = document.querySelector("#menu");
 
     menu.classList.add("d-block");
+
+    const isLoggedIn = load("profile");
+    const userProfile = await getUserProfile(isLoggedIn.name);
+    const profileLink = document.querySelector("#profileLink");
+    profileLink.href = `/profile/?name=${userProfile.name}`;
   });
 
   burgerClose.addEventListener("click", () => {
