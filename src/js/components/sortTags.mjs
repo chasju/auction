9ec78d498334bdf8289d115b43d52e6dbda6d4e4
chasju <91468816+chasju@users.noreply.github.com>
2022-12-13 14:59:52 +0100

    Adds sort tags function

---
+++ src/js/components/sortTags.mjs
@@ -0,0 +1,34 @@
+import { getPosts } from "../api/posts/get.mjs";
+
+export async function sortTags() {
+  const posts = await getPosts();
+  let tagsArray = [];
+  posts.forEach((post) => {
+    const tags = [...post.tags];
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i] === "") {
+        continue;
+      }
+      tagsArray.push(tags[i].trim());
+    }
+  });
+
+  const sortedArray = sortTagDesc(tagsArray);
+
+  return sortedArray;
+}
+
+export function sortTagDesc(array) {
+  const sortedArray = array.sort(function (a, b) {
+    const tagA = a.toLowerCase();
+    const tagB = b.toLowerCase();
+    if (tagA <= tagB) {
+      return -1;
+    }
+    if (tagA >= tagB) {
+      return 1;
+    }
+    return 0;
+  });
+  return sortedArray;
+}
