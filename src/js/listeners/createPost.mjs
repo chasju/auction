931a40490890cc3cecb7c2f13ccd979c81ba0b58
chasju <91468816+chasju@users.noreply.github.com>
2022-12-15 14:07:36 +0100

    Adds more images upload function

--- src/js/listeners/createPost.mjs
+++ src/js/listeners/createPost.mjs
@@ -18,7 +18,19 @@ export async function setCreatePostListener() {
       const formData = new FormData(form);
       const postData = Object.fromEntries(formData.entries());
       postData.tags = postData.tags.split(",").map((item) => item.trim());
-      postData.media = postData.media.split(",").map((item) => item.trim());
+      const imageInputs = document.querySelectorAll('input[name="media"]');
+      let mediaArray = [];
+
+      imageInputs.forEach((media) => {
+        if (media.value !== "") {
+          mediaArray.push(media.value);
+          console.log(mediaArray);
+        }
+      });
+
+      postData.media = mediaArray;
+
+      console.log(postData);
 
       await createPost(postData);
 
@@ -31,6 +43,7 @@ export async function setCreatePostListener() {
     const description = document.querySelector("#previewDescription");
     let image = document.querySelector("#previewImage");
     const deadline = document.querySelector("#previewTimer");
+    const addInputButtons = document.querySelectorAll(".addInput");
 
     form.addEventListener("change", (e) => {
       e.preventDefault();
@@ -46,6 +59,20 @@ export async function setCreatePostListener() {
       if (e.target.id === "deadline") {
         deadline.textContent = formatDate(form.endsAt.value);
       }
+    });
+
+    addInputButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const div = document.createElement("div");
+        const insertHere = document.querySelector("#deadlineDiv");
+        div.classList.add("form-floating", "mt-4", "input-group");
+
+        div.innerHTML = `<input type="url" name="media" class="form-control form-control-auth" id="image" placeholder="Image https://img.service.com/avatar.jpg"  />
+                <label for="image">Image <span class="fs-8 fst-italic">https://img.service.com/avatar.jpg</span></label>
+                `;
+
+        form.insertBefore(div, insertHere);
+      });
     });
   }
 }
